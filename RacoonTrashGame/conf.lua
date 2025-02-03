1d22cde96d60d49a791532b542dc543c3b8783f7
CosmicCynth <theshinyseal@gmail.com>
2025-02-03 16:35:02 +0100

    Update conf.lua

--- RacoonTrashGame/conf.lua
+++ RacoonTrashGame/conf.lua
@@ -2,3 +2,7 @@ function love.conf(t)
     t.window.title = "Racoon Shooters!"
     t.window.height = 800
 end
+
+--The player is equiped with limited shots and have to deliver trash to all the racoons on the apartment
+--mechanic: rebounce, Destory of trash bag, limited shots
+--dynamics: critical thinking, timing
